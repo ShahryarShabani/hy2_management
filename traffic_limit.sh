572f63e15eb3c0561b5a214c6e3bdc0dd05550f7
Shahryar <93078531+ShahryarShabani@users.noreply.github.com>
2024-02-27 02:54:16 +0330

    Update traffic_limit.sh
    Fixed bugs

--- traffic_limit.sh
+++ traffic_limit.sh
@@ -3,10 +3,10 @@
 DIR=$(realpath "$(dirname "$0")")
 
 Update_prev_traffic() {
-prev_rx=$(jq -r .$ID.rx $DIR/hysteria.json)
-new_rx=$(curl 'http://127.0.0.1:7687/traffic' | jq -r .$ID.rx)
-prev_tx=$(jq -r .$ID.tx $DIR/hysteria.json)
-new_tx=$(curl 'http://127.0.0.1:7687/traffic' | jq -r .$ID.tx)
+prev_rx=$(jq -r .$Id.rx $DIR/hysteria.json)
+new_rx=$(curl 'http://127.0.0.1:7687/traffic' | jq -r .$Id.rx)
+prev_tx=$(jq -r .$Id.tx $DIR/hysteria.json)
+new_tx=$(curl 'http://127.0.0.1:7687/traffic' | jq -r .$Id.tx)
 # compare the rx values and update them if they are lower
 if [ $prev_rx == "null" ] ; then
 prev_rx=0
@@ -16,8 +16,8 @@ prev_tx=0
 fi
 if [ $new_rx != "null" ]; then
 # update the rx value of amirali in prev.json
-jq --arg name "$ID" --arg new_rx_edit "$((prev_rx + new_rx))" '.[$name].rx = $new_rx_edit' $DIR/hysteria.json | sponge $DIR/hysteria.json
-jq --arg name "$ID" --arg new_tx_edit "$((prev_tx + new_tx))" '.[$name].tx = $new_tx_edit' $DIR/hysteria.json | sponge $DIR/hysteria.json
+jq --arg name "$Id" --arg new_rx_edit "$((prev_rx + new_rx))" '.[$name].rx = $new_rx_edit' $DIR/hysteria.json | sponge $DIR/hysteria.json
+jq --arg name "$Id" --arg new_tx_edit "$((prev_tx + new_tx))" '.[$name].tx = $new_tx_edit' $DIR/hysteria.json | sponge $DIR/hysteria.json
 else
 echo "no updates"
 fi
@@ -25,7 +25,7 @@ fi
 
 
 
-IDs=$(jq -r 'keys[]' $DIr/hysteriadb.json)
+IDs=$(yq -r '.auth.userpass | keys[]' /etc/hysteria/config.yaml)
 for ID in $IDs
 do
 echo "-------------------------------------"
@@ -47,7 +47,11 @@ traffic=$(($new_traffic + $prev_traffic))
 fi
 if [ $traffic_limit != "null" ] && [ "$traffic" -ge "$traffic_limit" ]; then
 yq "del(.auth.userpass.$ID)" /etc/hysteria/config.yaml -y -i
+IDss=$(yq -r '.auth.userpass | keys[]' /etc/hysteria/config.yaml)
+for Id in $IDss
+do
 Update_prev_traffic
+done
 jq --arg name "$ID" --arg new_rx_edit "0" '.[$name].rx = $new_rx_edit' $DIR/hysteria.json | sponge $DIR/hysteria.json
 jq --arg name "$ID" --arg new_tx_edit "0" '.[$name].tx = $new_tx_edit' $DIR/hysteria.json | sponge $DIR/hysteria.json
 systemctl restart hysteria-server.service
@@ -65,7 +69,11 @@ expired=$(date -d "$end_date" +%s)
 now=$(date +%s)
 if [ "$expired" -le $now ]; then
 yq "del(.auth.userpass.$ID)" /etc/hysteria/config.yaml -y -i
+IDss=$(yq -r '.auth.userpass | keys[]' /etc/hysteria/config.yaml)
+for Id in $IDss
+do
 Update_prev_traffic
+done
 jq --arg name "$ID" --arg new_rx_edit "0" '.[$name].rx = $new_rx_edit' $DIR/hysteria.json | sponge $DIR/hysteria.json
 jq --arg name "$ID" --arg new_tx_edit "0" '.[$name].tx = $new_tx_edit' $DIR/hysteria.json | sponge $DIR/hysteria.json
 systemctl restart hysteria-server.service
